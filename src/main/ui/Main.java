92c39a65335f350e696b16c3dd5c90c911e205b3
ding <dingthema@gmail.com>
2021-11-18 21:39:23 -0800

    Implemented GUI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,26 +1,16 @@
 package ui;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 // main interface class
 // This class references code from CPSC 210 JsonSerializationDemo
 // Link: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    GraphicalApp.createAndShowGUI();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-//        try {
-//            new AnimeListApp();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Unable to run application: file not found");
-//        }
+        try {
+            new AnimeListApp();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to run application: file not found");
+        }
     }
 }
