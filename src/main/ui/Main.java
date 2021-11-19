eb6332fc13e15fe71721d55d8c32e10f093b5e41
ding <dingthema@gmail.com>
2021-11-18 21:40:51 -0800

    Revert "Implemented GUI"
    This reverts commit 92c39a65335f350e696b16c3dd5c90c911e205b3.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,16 +1,26 @@
 package ui;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 // main interface class
 // This class references code from CPSC 210 JsonSerializationDemo
 // Link: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public class Main {
     public static void main(String[] args) {
-        try {
-            new AnimeListApp();
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to run application: file not found");
-        }
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    GraphicalApp.createAndShowGUI();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+//        try {
+//            new AnimeListApp();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Unable to run application: file not found");
+//        }
     }
 }
